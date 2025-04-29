be34a20f84d03b2fd9449aa21c861b4bdc54f8f8
K C <164186688+heykinchan@users.noreply.github.com>
2025-04-29 11:46:49 +1000

    updated the layout to customise for sc

--- frontend/src/layout/AppSidebar.tsx
+++ frontend/src/layout/AppSidebar.tsx
@@ -299,14 +299,14 @@ const AppSidebar: React.FC = () => {
             <>
               <Image
                 className="dark:hidden"
-                src="/images/logo/logo.svg"
+                src="/images/logo/sc-logo-long-dark.svg"
                 alt="Logo"
                 width={150}
                 height={40}
               />
               <Image
                 className="hidden dark:block"
-                src="/images/logo/logo-dark.svg"
+                src="/images/logo/sc-logo-long-light.svg"
                 alt="Logo"
                 width={150}
                 height={40}
@@ -314,7 +314,7 @@ const AppSidebar: React.FC = () => {
             </>
           ) : (
             <Image
-              src="/images/logo/logo-icon.svg"
+              src="/images/logo/sc-logo-long-light.svg"
               alt="Logo"
               width={32}
               height={32}
